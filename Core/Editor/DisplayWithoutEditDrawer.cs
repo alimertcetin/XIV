35d2b7323a1a09166d6348cd7c905ac7bfb7cd61
alimertcetin <mrtmert54@gmail.com>
2023-05-17 23:16:36 +0300

    DisplayWithoutEdit update
    Added support for
    - AnimationCurve
    - Bool
    - Bounds
    - Color
    - LayerMaksk
    - ObjectReference
    - Quaternion
    - Rect
    - String
    - Vector2
    - Vector3
    - Vector4

--- Core/Editor/DisplayWithoutEditDrawer.cs
+++ Core/Editor/DisplayWithoutEditDrawer.cs
@@ -1,33 +1,34 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using XIV.Core;
-using XIV.XIVEditor.Utils;
 
 namespace XIV.XIVEditor
 {
     [CustomPropertyDrawer(typeof(DisplayWithoutEdit))]
     public class DisplayWithoutEditDrawer : PropertyDrawer
     {
-        /// <summary>
-        /// Display attribute and his value in inspector depending on the type
-        /// Fill attribute needed
-        /// </summary>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            GUI.enabled = false;
             switch (property.propertyType)
             {
                 case SerializedPropertyType.AnimationCurve:
+                    EditorGUI.CurveField(position, label, property.animationCurveValue);
                     break;
                 case SerializedPropertyType.ArraySize:
+                    base.OnGUI(position, property, label);
                     break;
                 case SerializedPropertyType.Boolean:
-                    EditorGUI.LabelField(position, label, new GUIContent(property.boolValue.ToString()));
+                    EditorGUI.Toggle(position, label, property.boolValue);
                     break;
                 case SerializedPropertyType.Bounds:
+                    EditorGUI.LabelField(position, label.ToString(), property.boundsValue.ToString());
                     break;
                 case SerializedPropertyType.Character:
+                    base.OnGUI(position, property, label);
                     break;
                 case SerializedPropertyType.Color:
+                    EditorGUI.ColorField(position, label, property.colorValue);
                     break;
                 case SerializedPropertyType.Enum:
                     EditorGUI.LabelField(position, label, new GUIContent(property.enumDisplayNames[property.enumValueIndex]));
@@ -36,30 +37,40 @@ namespace XIV.XIVEditor
                     EditorGUI.LabelField(position, label, new GUIContent(property.floatValue.ToString()));
                     break;
                 case SerializedPropertyType.Generic:
+                    base.OnGUI(position, property, label);
                     break;
                 case SerializedPropertyType.Gradient:
+                    base.OnGUI(position, property, label);
                     break;
                 case SerializedPropertyType.Integer:
                     EditorGUI.LabelField(position, label, new GUIContent(property.intValue.ToString()));
                     break;
                 case SerializedPropertyType.LayerMask:
+                    EditorGUI.LayerField(position, label, property.intValue);
                     break;
                 case SerializedPropertyType.ObjectReference:
+                    EditorGUI.ObjectField(position, property, label);
                     break;
                 case SerializedPropertyType.Quaternion:
+                    EditorGUI.Vector4Field(position, label, new Vector4(property.quaternionValue.x, property.quaternionValue.y, property.quaternionValue.z, property.quaternionValue.w));
                     break;
                 case SerializedPropertyType.Rect:
+                    EditorGUI.LabelField(position, label.ToString(), property.rectValue.ToString());
                     break;
                 case SerializedPropertyType.String:
-                    EditorGUI.LabelField(position, label, new GUIContent(property.stringValue));
+                    EditorGUI.LabelField(position, label.ToString(), property.stringValue);
                     break;
                 case SerializedPropertyType.Vector2:
+                    EditorGUI.Vector2Field(position, label, new Vector2(property.vector2Value.x, property.vector2Value.y));
                     break;
                 case SerializedPropertyType.Vector3:
+                    EditorGUI.Vector3Field(position, label, new Vector3(property.vector3Value.x, property.vector3Value.y, property.vector3Value.z));
                     break;
                 case SerializedPropertyType.Vector4:
+                    EditorGUI.Vector4Field(position, label, new Vector4(property.vector4Value.x, property.vector4Value.y, property.vector4Value.z, property.vector4Value.w));
                     break;
             }
+            GUI.enabled = true;
         }
     }
 }
