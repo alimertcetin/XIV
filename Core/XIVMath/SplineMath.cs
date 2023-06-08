ecb19f889c9e398ab6a79327b4c999ff7856407d
alimertcetin <mrtmert54@gmail.com>
2023-06-08 13:20:51 +0300

    SplineMath update
    Now we can get t from spline depending on position

--- Core/XIVMath/SplineMath.cs
+++ Core/XIVMath/SplineMath.cs
@@ -33,7 +33,37 @@ namespace XIV.Core.XIVMath
 
 			return BezierMath.GetPoint(points[index], points[index + 1], points[index + 2], points[index + 3], t);
 		}
-        
+
+		public static float GetTime(Vector3 currentPosition, IList<Vector3> points, float tolarence = 0.01f)
+		{
+			int curveCount = (points.Count - 1) / 3;
+			float minDistance = float.MaxValue;
+			float t = 0f;
+
+			for (int i = 0; i < curveCount; i++)
+			{
+				int index = i * 3;
+				Vector3 p0 = points[index];
+				Vector3 p1 = points[index + 1];
+				Vector3 p2 = points[index + 2];
+				Vector3 p3 = points[index + 3];
+
+				for (float j = 0f; j <= 1f; j += tolarence)
+				{
+					Vector3 curvePosition = BezierMath.GetPoint(p0, p1, p2, p3, j);
+					float distance = Vector3.Distance(curvePosition, currentPosition);
+					
+					if (distance < minDistance)
+					{
+						minDistance = distance;
+						t = (i + j) / curveCount; // Calculate the t value relative to the entire curve
+					}
+				}
+			}
+
+			return t;
+		}
+
 		/// <summary>
 		/// Returns the Velocity of spline at <paramref name="t"/> point
 		/// </summary>
