d23ec2f35570794e23406683282ce4d81278a538
alimertcetin <mrtmert54@gmail.com>
2025-06-06 22:50:53 +0300

    feat(StringExtensions): Add TruncateWithChar

--- Core/Extensions/StringExtensions.cs
+++ Core/Extensions/StringExtensions.cs
@@ -14,5 +14,20 @@
             int padLeft = padding / 2 + str.Length;
             return str.PadLeft(padLeft).PadRight(width);
         }
+
+        public static string TruncateWithDots(this string str, int maxLength)
+        {
+            return TruncateWithChar(str, maxLength, '.');
+        }
+
+        public static string TruncateWithChar(this string str, int maxLength, char c)
+        {
+            if (string.IsNullOrEmpty(str) || maxLength < 4)
+                return (str ?? "").PadRight(maxLength); // fallback
+
+            return str.Length > maxLength
+                ? str.Substring(0, maxLength - 3) + new string(c, 3)
+                : str;
+        }
     }
 }
