574f88e90ecfbbb2e83001dd30c5bff5eb320b29
alimertcetin <mrtmert54@gmail.com>
2025-06-06 22:50:15 +0300

    feat(StringExtensions): Add PadCenter

--- Core/Extensions/StringExtensions.cs
+++ Core/Extensions/StringExtensions.cs
@@ -6,5 +6,13 @@
         {
             return value + " ";
         }
+
+        public static string PadCenter(this string str, int width)
+        {
+            if (string.IsNullOrEmpty(str)) str = "";
+            int padding = width - str.Length;
+            int padLeft = padding / 2 + str.Length;
+            return str.PadLeft(padLeft).PadRight(width);
+        }
     }
 }
