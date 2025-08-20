dd7f656ea50aa2b8203573428d324348f0cf3631
alimertcetin <mrtmert54@gmail.com>
2025-08-20 06:17:08 +0300

    feat(ArrayUtils): Add Merge

--- Core/Utils/ArrayUtils.cs
+++ Core/Utils/ArrayUtils.cs
@@ -4,6 +4,26 @@ namespace XIV.Core.Utils
 {
     public static class ArrayUtils
     {
+        public static int Merge<T>(T[] a, int aLen, T[] b, int bLen, T[] buffer)
+        {
+            int bufferLen = buffer.Length;
+            var mergedCount = 0;
+            T def = default(T);
+            for (var i = 0; i < aLen && mergedCount < bufferLen; i++)
+            {
+                if (a[i].Equals(def)) continue;
+                buffer[mergedCount++] = a[i];
+            }
+
+            for (var i = 0; i < bLen && mergedCount < bufferLen; i++)
+            {
+                if (b[i].Equals(def)) continue;
+                buffer[mergedCount++] = b[i];
+            }
+
+            return mergedCount;
+        }
+        
         // https://en.wikipedia.org/wiki/Row-_and_column-major_order
         public static int Get1DIndex(int x, int y, int width)
         {
