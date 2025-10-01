a72d3c97b6a0539bfc3504219e1a90b69bed27c9
alimertcetin <mrtmert54@gmail.com>
2025-10-01 18:30:28 +0300

    feat(XIVMathInt): Add NextPowerOfTwo function

--- Runtime/Core/XIVMath/XIVMathInt.cs
+++ Runtime/Core/XIVMath/XIVMathInt.cs
@@ -21,5 +21,28 @@
         {
 			return val > max ? max : val < min ? min : val;
         }
-    }
+
+        /// <summary>
+        /// Calculates the next power of two of <paramref name="num"/>
+        /// </summary>
+        /// <param name="num">The number to calculate next power of two</param>
+        /// <returns>Returns the <paramref name="num"/> if it is already power of two, otherwise returns the next power of two</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int NextPowerOfTwo(int num)
+        {
+	        num--;
+	        num |= num >> 1;
+	        num |= num >> 2;
+	        num |= num >> 4;
+	        num |= num >> 8;
+	        num |= num >> 16;
+	        return num + 1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPowerOfTwo(int x)
+        {
+	        return (x & (x - 1)) == 0;
+        }
+	}
 }
