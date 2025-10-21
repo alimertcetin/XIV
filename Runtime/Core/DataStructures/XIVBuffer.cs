e7503c3803fe5cdf67f4a9520aaa98155c691f38
alimertcetin <mrtmert54@gmail.com>
2025-10-21 20:06:54 +0300

    feat(XIVBuffer): Add XIVBuffer to dispose buffers without tracking

---
+++ Runtime/Core/DataStructures/XIVBuffer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Buffers;
+
+namespace XIV.Core.DataStructures
+{
+    public struct XIVBuffer<T> : IDisposable
+    {
+        readonly T[] buffer;
+
+        XIVBuffer(T[] buffer)
+        {
+            this.buffer = buffer;
+        }
+        
+        internal static XIVBuffer<T> Get(int minLength, out T[] buffer)
+        {
+            buffer = ArrayPool<T>.Shared.Rent(minLength);
+            var xivBuffer = new XIVBuffer<T>(buffer);
+            return xivBuffer;
+        }
+
+        void IDisposable.Dispose()
+        {
+            ArrayPool<T>.Shared.Return(buffer);
+        }
+    }
+}
