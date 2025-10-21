805078d8f77e2f0714ca2fa8017896902a4674f2
alimertcetin <mrtmert54@gmail.com>
2025-10-21 19:57:56 +0300

    feat(XIVMathInt): Add Abs

--- Runtime/Core/XIVMath/XIVMathInt.cs
+++ Runtime/Core/XIVMath/XIVMathInt.cs
@@ -50,5 +50,11 @@ namespace XIV.Core.XIVMath
         {
 	        return (x & (x - 1)) == 0;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Abs(int x)
+        {
+	        return (x < 0) ? -x : x;
+        }
 	}
 }
